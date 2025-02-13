54101c2d4ab8b35e25d15df505a55e0753a96e58
Rafael <rafaelbortoli21@gmail.com>
2025-02-13 16:49:15 -0300

    cors

---
+++ backend-java/src/main/java/com/show_rural/hackathon/config/WebConfiguration.java
@@ -0,0 +1,24 @@
+package com.show_rural.hackathon.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class WebConfiguration {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.addAllowedOrigin("*");
+        config.addAllowedMethod("*");
+        config.addAllowedHeader("*");
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsFilter(source);
+    }
+}
